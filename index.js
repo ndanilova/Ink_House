8f878943eba963e479158d6827d324ab6879d24c
ndanilova <70074699+ndanilova@users.noreply.github.com>
2024-11-07 02:46:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".countries button");
+  const sections = document.querySelectorAll(".country-section");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const country = button.getAttribute("data-country");
+
+      // Убираем класс active со всех кнопок
+      buttons.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+
+      // Добавляем класс active нажатой кнопке
+      button.classList.add("active");
+
+      // Скрываем все секции
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+
+      // Показываем нужную секцию
+      const activeSection = document.querySelector(`.${country}`);
+      activeSection.classList.add("active");
+    });
+  });
+});
